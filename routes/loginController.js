e6001829d2abac6344023647f277fa3f14ee34ea
Azzaid <johanas.azzaid@gmail.com>
2019-05-19 22:26:19 +0300

    Some tweaks to log errors

--- routes/loginController.js
+++ routes/loginController.js
@@ -6,26 +6,29 @@ const mongoClient = new mongodb.MongoClient(uri);
 
 const router = express.Router();
 
-router.get('/new', function(req, res){
+router.get('/new', function(request, response){
   mongoClient.connect(function(err, client){
     
     if(err){
       client.close();
+      response.status(500).send(err);
       return console.log(err);
     }
     
     const db = client.db("usersdb");
     const collection = db.collection("users");
     
-    let user = {login: req.body.userName, password: req.body.passWord};
+    let user = {login: request.body.userName, password: request.body.passWord};
     
     collection.insertOne(user, function(err, result){
       
       if(err){
         client.close();
+        response.status(500).send(err);
         return console.log(err);
       }
       console.log(result.ops);
+      response.status(200).send(result['_id']);
     });
     
     client.close();
